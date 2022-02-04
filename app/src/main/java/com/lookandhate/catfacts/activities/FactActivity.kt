3e785b927021c3f5c6d99f999b85f0ced25af607
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-02-04 22:41:24 +0400

    Removed unused import statements. Added padding to text at fact page

--- app/src/main/java/com/lookandhate/catfacts/activities/FactActivity.kt
+++ app/src/main/java/com/lookandhate/catfacts/activities/FactActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -20,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
-import coil.request.ImageRequest.Builder.*
 import com.lookandhate.catfacts.AppMain
 import com.lookandhate.catfacts.R
 import com.lookandhate.catfacts.MainActivity
@@ -118,7 +116,8 @@ fun FactPage(factToDispay: Fact?, imageUrl: String?) {
     val checkedState = remember { mutableStateOf(fact.isFavorite) }
     val factIndexInArray = AppMain.getFactIndexByItsText(fact.factText)
     Column() {
-        Text(text = fact.factText)
+        Text(text = fact.factText,
+        modifier = Modifier.padding(5.dp))
 
         Image(
             painter = rememberImagePainter(imageUrl,
