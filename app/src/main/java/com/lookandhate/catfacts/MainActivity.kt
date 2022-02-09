7face3a62a594e78a71ee1487ce1affc03ad6f15
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-02-09 16:28:04 +0400

    Implemented proper navigation bar

--- app/src/main/java/com/lookandhate/catfacts/MainActivity.kt
+++ app/src/main/java/com/lookandhate/catfacts/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -14,7 +16,14 @@ import androidx.compose.material.icons.filled.Favorite
 
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 import com.lookandhate.catfacts.composables.FactsFromTheNetComposable
 import com.lookandhate.catfacts.composables.FavoritesComposable
@@ -31,11 +40,13 @@ object AppMain {
         return -1
     }
 
-    const val factsFromTheNetPageTag = "download"
-    const val favoritesFactsPageTag = "favorites"
-    val pageToDisplay = mutableStateOf<String>(factsFromTheNetPageTag)
-
 }
+
+sealed class Screen(val route: String, @StringRes val screenLabelResource: Int, val icon: ImageVector) {
+    object Download : Screen("download", R.string.facts_from_net_page, Icons.Filled.ArrowDropDown)
+    object Favorites : Screen("favorites", R.string.favorites_facts_page, Icons.Filled.Favorite)
+}
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,43 +67,49 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    val selectedItem = remember { mutableStateOf(AppMain.pageToDisplay.value) }
+    val navController = rememberNavController()
+    val navigationScreens = listOf(
+        Screen.Download,
+        Screen.Favorites
+    )
+
     Scaffold(
         bottomBar = {
             BottomAppBar {
                 BottomNavigation() {
-                    BottomNavigationItem(
-                        icon = {
-                            Icon(Icons.Filled.ArrowDropDown, "")
-                        },
-                        label = { Text(text = stringResource(R.string.facts_from_net_page)) },
-                        selected = selectedItem.value == AppMain.factsFromTheNetPageTag,
-                        onClick = {
-                            selectedItem.value = AppMain.factsFromTheNetPageTag
-                        },
-                        alwaysShowLabel = false
-                    )
-
-                    BottomNavigationItem(
-                        icon = {
-                            Icon(Icons.Filled.Favorite, "")
-                        },
-                        label = { Text(text = stringResource(R.string.favorites_facts)) },
-                        onClick = {
-                            selectedItem.value = AppMain.favoritesFactsPageTag
-                        },
-                        selected = selectedItem.value == AppMain.favoritesFactsPageTag,
-                        alwaysShowLabel = false
-                    )
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentDestination = navBackStackEntry?.destination
+                    navigationScreens.forEach { screen ->
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(screen.icon, null)
+                            },
+                            label = { Text(text = stringResource(screen.screenLabelResource)) },
+                            selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                            onClick = {
+                                navController.navigate(screen.route) {
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                            },
+                            alwaysShowLabel = true
+                        )
+                    }
                 }
             }
-        },
-        content = {
-            if (selectedItem.value == AppMain.factsFromTheNetPageTag) {
-                FactsFromTheNetComposable()
-            } else if (selectedItem.value == AppMain.favoritesFactsPageTag) {
-                FavoritesComposable()
-            }
         }
-    )
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = Screen.Download.route,
+            Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Download.route) { FactsFromTheNetComposable() }
+            composable(Screen.Favorites.route) { FavoritesComposable() }
+        }
+    }
+
 }
